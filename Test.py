92aec6244f6806a72d43454491e2f529fa5b5ac5
phucnguyen1707 <phucnguyen17072001@gmail.com>
2022-10-21 19:10:24 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -65,3 +65,7 @@ for _data_name in ['CVC-300', 'CVC-ClinicDB', 'Kvasir', 'CVC-ColonDB', 'ETIS-Lar
         res = (res - res.min()) / (res.max() - res.min() + 1e-8)
         
         misc.imsave(save_path+name, res)
+
+
+# a[ousgfbwdow
+# d]
